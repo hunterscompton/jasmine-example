6ec9034e4f9cbd0f814a0fe0e0a1cf2941b60994
Adam <adamabe123@gmail.com>
2018-03-12 21:29:09 -0400

    added dob utlilty

--- spec/UtilitySpec.js
+++ spec/UtilitySpec.js
@@ -1,7 +1,14 @@
-describe("utility functions", function () {
-  describe("fullName()", function() {
+describe("utility functions", function() {
+  describe("fullName", function() {
     it("Concatenates two name parts with a space", function() {
       expect(fullName("Billy", "Bob")).toEqual("Billy Bob");
     });
   });
-});
+
+
+  describe("ageInYears", () => {
+    it("returns the age from a birthday", () => {
+      expect(ageInYears("1980-05-17")).toEqual(37);
+    })
+  });
+})
